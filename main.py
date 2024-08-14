c4bdf7209a3efb0b9947ed2ba2a8c17e5e907192
jihwanK-ufo-desk <computerandgyein@gmail.com>
2024-08-14 15:39:01 +0900

    [Feat] Add main function

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from logger import Logger
+from chatbot import Chatbot
+
+def main():
+    logger = Logger(os.getenv('LOG_LEVEL'))
+
+    logger.info("[main] Start")
+
+    # chatbot = Chatbot(logger, 'pooh')
+    chatbot = Chatbot()
+    chatbot.run()
+
+
+if __name__ == '__main__':
+    main()
