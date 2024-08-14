b94932bf94be33d261d7e598cd98d8989fd602a9
jihwanK-ufo-desk <computerandgyein@gmail.com>
2024-08-14 15:38:15 +0900

    [Feat] Add Chatbot module

---
+++ chatbot.py
@@ -0,0 +1,26 @@
+import os
+from logger import Logger
+
+
+class Chatbot():
+    def __init__(self, member='all'):
+        self.logger = Logger(os.getenv('LOG_LEVEL'))
+        self.member = member.lower()
+
+        if self.member == 'all':
+            pass
+        elif self.member == 'pooh':
+            pass
+        elif self.member == 'tiger':
+            pass
+        elif self.member == 'piglet':
+            pass
+        elif self.member == 'eeyore':
+            pass
+        else:
+            pass
+
+        self.logger.info("[Chatbot] Chatbot system initiated")
+
+    def run(self):
+        self.logger.info("[Chatbot] Chatbot system is running.")
