60c09b5bf8699287df1fc784fc52b7d580e07f93
tjgusKim <ashley5017@naver.com>
2024-08-17 12:26:55 +0900

    [Fix] Change memory type

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ import os
 from operator import itemgetter
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 
 from logger import Logger
 from llm import LLM
@@ -18,7 +18,8 @@ class Chatbot():
         self.llm = LLM().create_llm(llm_platform)
         self.retriever = self.vector_store.get_retriever()
         self.prompt = prompt.get_prompt()
-        self.memory = ConversationBufferMemory(
+        self.memory = ConversationBufferWindowMemory(
+            k=25,
             ai_prefix="Pooh and his friends",
             human_prefix="User"
         )
